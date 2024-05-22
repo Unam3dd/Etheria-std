991aa5ff11f590ca6064d8f728331f637684d879
Unam3dd <samtalespro@gmail.com>
2024-05-22 12:41:32 +0200

    update: taskfile and some lines in meson build

--- src/memory/memcpy/memcpy.c
+++ src/memory/memcpy/memcpy.c
@@ -6,20 +6,24 @@
 //
 //////////////////////////////////////
 
-static void *_eth_memcpy_naive(void *dst, const void *src, size_t size)
+static void *
+_eth_memcpy_naive (void *dst, const void *src, size_t size)
 {
-    if (!dst || !src || !size) return (dst);
-
-    u8_t *psrc = (u8_t*)src;
-    u8_t *pdst = (u8_t *)dst;
-
-    while ((size > 0) && size--) *pdst++ = *psrc++;
+  if (!dst || !src || !size)
     return (dst);
+
+  u8_t *psrc = (u8_t *) src;
+  u8_t *pdst = (u8_t *) dst;
+
+  while ((size > 0) && size--)
+    *pdst++ = *psrc++;
+  return (dst);
 }
 
-void * (*_eth_memcpy_ifunc (void))(void *, const void *, eth_size_t)
+void *(*_eth_memcpy_ifunc (void)) (void *, const void *, size_t)
 {
-    return (_eth_memcpy_naive);
+  return (_eth_memcpy_naive);
 }
 
-void    *eth_memcpy(void *dst, const void *src, eth_size_t size) __attribute__((ifunc("_eth_memcpy_ifunc")));
+void *eth_memcpy (void *dst, const void *src, size_t size)
+  __attribute__((ifunc ("_eth_memcpy_ifunc")));
