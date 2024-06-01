e8d330b15dcc0d9f6c6f4641196db65a3984310b
Unam3dd <samtalespro@gmail.com>
2024-06-01 22:55:09 +0200

    update naive memcpy im so stupid orhh

--- src/memory/memcpy/memcpy.c
+++ src/memory/memcpy/memcpy.c
@@ -6,8 +6,6 @@
 //
 //////////////////////////////////////
 
-#if defined(__linux__)
-
 STATIC void *_eth_memcpy_naive (void *restrict dst, const void *restrict src, size_t size)
 {
   if (!dst || !src || !size)
@@ -20,6 +18,8 @@ STATIC void *_eth_memcpy_naive (void *restrict dst, const void *restrict src, si
   return (dst);
 }
 
+#if defined(__linux__)
+
 void *(*_eth_memcpy_ifunc (void)) (void *restrict, const void *restrict, size_t)
 {
   return (_eth_memcpy_naive);
@@ -27,18 +27,5 @@ void *(*_eth_memcpy_ifunc (void)) (void *restrict, const void *restrict, size_t)
 
 void *eth_memcpy (void *restrict dst, const void *restrict src, size_t size) __attribute__((ifunc ("_eth_memcpy_ifunc")));
 
-#else
-
-void * _eth_memcpy_naive (void *restrict dst, const void *restrict src, size_t size)
-{
-  if (!dst || !src || !size)
-    return (dst);
-
-  u8_t *psrc = (u8_t *) src;
-  u8_t *pdst = (u8_t *) dst;
-
-  while (size--) *pdst++ = *psrc++;
-  return (dst);
-}
 
 #endif
